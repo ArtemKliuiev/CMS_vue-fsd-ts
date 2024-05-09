b3e0411c32b00b3f58db2a89bd9df0d45d379f3d
artem <mr_artem>
2024-05-09 11:51:25 +0300

    setting router

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 const router = createRouter({
-  history: createWebHistory('kino-cms'),
+  history: createWebHistory(''),
   routes: [
     {
       path: '/',
