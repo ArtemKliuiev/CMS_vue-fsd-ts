4e2206fd15e64cdd8335078166f64fc4d69dfa43
artem <mr_artem>
2024-05-09 12:23:30 +0300

    setting

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'kino-cms',
+  base: '/kino-cms',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
