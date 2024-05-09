c77a26fb74233bc6bad67efd7011bc92954840eb
artem <mr_artem>
2024-05-09 10:19:27 +0300

    add

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Home 222222222222222</RouterLink>
+        <RouterLink to="/about">About 2222222222222</RouterLink>
       </nav>
     </div>
   </header>
