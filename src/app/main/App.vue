2fd5b1b0f62121b77108791f5eab5bb09354981a
artem <mr.kluev9999@gmail.com>
2024-05-13 12:02:45 +0300

    setting

--- src/app/main/App.vue
+++ src/app/main/App.vue
@@ -3,7 +3,7 @@
 
     <router-view></router-view>
 
-    <router-link to="/">1</router-link>
+    <router-link to="/">1.</router-link>
     <router-link to="/2">2</router-link>
   </div>
 </template>
