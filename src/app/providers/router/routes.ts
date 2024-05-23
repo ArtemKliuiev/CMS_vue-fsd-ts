2e2c4232002390d583573e71adca5bc0d9319d77
artem <mr.kluev9999@gmail.com>
2024-05-23 16:22:25 +0300

    resolve conflicts

--- src/app/providers/router/routes.ts
+++ src/app/providers/router/routes.ts
@@ -4,27 +4,16 @@ import { AdminLayout, WebsiteLayout } from '@/shared/ui'
 import {
   BannersPage,
   CinemasPage,
-  CinemasCardHall,
-  CinemasCardCinema,
   MoviesPage,
   MovieCreate,
   NewsPage,
-  NewsCreate,
   NewsletterPage,
   PagesPage,
-  HomePage,
-  AboutCinemaPage,
-  CafeBar,
   StatisticsPage,
   StockPage,
-<<<<<<< HEAD
-  StockCreate,
-  UsersPage
-=======
   UsersPage,
   AuthPage,
   RegistrationPage
->>>>>>> artem-dev
 } from '@/pages'
 
 export const routes = [
@@ -53,127 +42,6 @@ export const routes = [
     }
   },
   {
-<<<<<<< HEAD
-    path: '/admin/pages/home-page',
-    name: 'home-page',
-    component: HomePage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/pages/about-cinema-page',
-    name: 'about-cinema-page',
-    component: AboutCinemaPage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/pages/cafe-bar',
-    name: 'cafe-bar',
-    component: CafeBar,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/banners',
-    name: 'banners',
-    component: BannersPage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/news',
-    name: 'news',
-    component: NewsPage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/news-create',
-    name: 'news-create',
-    component: NewsCreate,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/movies',
-    name: 'movies',
-    component: MoviesPage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/movies-create',
-    name: 'movies-create',
-    component: MovieCreate,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/cinemas',
-    name: 'cinemas',
-    component: CinemasPage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/cinemas-card-cinema',
-    name: 'cinemas-card-cinema',
-    component: CinemasCardCinema,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/cinemas-card-hall',
-    name: 'cinemas-card-hall',
-    component: CinemasCardHall,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-
-  {
-    path: '/admin/newsletter',
-    name: 'newsletter',
-    component: NewsletterPage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/stock',
-    name: 'stock',
-    component: StockPage,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/stock-create',
-    name: 'stock-create',
-    component: StockCreate,
-    meta: {
-      layout: AdminLayout
-    }
-  },
-  {
-    path: '/admin/users',
-    name: 'users',
-    component: UsersPage,
-    meta: {
-      layout: AdminLayout
-    }
-=======
     path: '/admin',
     meta: {
       layout: AdminLayout
@@ -230,6 +98,5 @@ export const routes = [
         component: UsersPage
       }
     ]
->>>>>>> artem-dev
   }
 ]
